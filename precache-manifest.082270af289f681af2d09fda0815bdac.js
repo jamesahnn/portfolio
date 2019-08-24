e12cf1ba738a1b3a9775e8f471432950de105360
James <jamesahn0717@gmail.com>
2019-08-23 21:25:30 -0700

    Updates

---
+++ precache-manifest.082270af289f681af2d09fda0815bdac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631d50e507ee1693b7080078ae603b7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2917daac4e6ec3be339",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f80f0acbd2d0ce47e57c",
+    "url": "/static/js/2.917ae229.chunk.js"
+  },
+  {
+    "revision": "b2917daac4e6ec3be339",
+    "url": "/static/js/main.b98d1a75.chunk.js"
+  },
+  {
+    "revision": "7c23a43b52e067a1c7c0",
+    "url": "/static/js/runtime~main.f6dce284.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
